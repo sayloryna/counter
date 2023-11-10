9579332b9386c97f2ea1ae1e920446534f409565
sayloryna <128187875+sayloryna@users.noreply.github.com>
2023-11-10 17:31:37 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+
+let value = 0 //contador
+
+//incrementar uno
+function  incrementarUno(num){
+   if(num >= 10){
+    return 10;
+    };
+    return num +1;
+    
+
+}
+//sustraer UNO
+function sustraerUno(num){
+    if(num <=10){
+        return -10;
+    }
+    return num - 1;
+}
+//set a zero
+function setToZero(){
+    return 0;
+}
+
+document.querySelector(.boton__sustraer(){
+    sustraerUno
+
+}
+
+
+
